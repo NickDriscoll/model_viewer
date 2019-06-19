d23e54e0ead28968b4351f311bc8d4ec6435f81c
Nick Driscoll <nick.a.mavros@gmail.com>
2019-06-18 23:59:50 -0400

    Added specular lighting to the fragment shader

--- shaders/nonluminous_fragment.glsl
+++ shaders/nonluminous_fragment.glsl
@@ -7,11 +7,11 @@ out vec4 color;
 
 uniform sampler2D tex;
 uniform vec4 light_position;
+uniform vec4 view_position;
 
-const float angle = 90; //Angle is in degrees
 const vec3 LIGHT_COLOR = vec3(1.0, 1.0, 1.0);
-//const vec4 LIGHT_POSITION = vec4(0.0, 0.5, 0.0, 1.0);
 const float AMBIENT_STRENGTH = 0.1;
+const float SHININESS = 16.0;
 
 void main() {
 	//Normalize any vectors
@@ -30,8 +30,12 @@ void main() {
 	float diff = max(0.0, dot(norm, light_direction));
 	vec3 diffuse = diff * LIGHT_COLOR;
 
-	//Get specular contribution
+	//Get specular contribution (blinn-phong)
+	vec4 view_direction = normalize(view_position - f_pos);
+	vec4 half_dir = normalize(light_direction + view_direction);
+	float specular_angle = max(0.0, dot(norm, half_dir));
+	vec3 specular = pow(specular_angle, SHININESS) * LIGHT_COLOR;
 
-	vec3 result = tex_color * (ambient + diffuse);
+	vec3 result = tex_color * (ambient + diffuse + specular);
 	color = vec4(result, 1.0);
 }
